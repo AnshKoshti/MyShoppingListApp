7724bb15d535f14693e7c5159aed25d0ae44a688
AnshKoshti <koshtiwork26@gmail.com>
2023-12-21 11:19:28 +0530

    Final cleanup.

--- app/src/main/java/eu/tutorials/myshoppinglistapp/LocationSelectionScreen.kt
+++ app/src/main/java/eu/tutorials/myshoppinglistapp/LocationSelectionScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -21,26 +20,29 @@ import com.google.maps.android.compose.rememberCameraPositionState
 @Composable
 fun LocationSelectionScreen(
     location: LocationData,
-    onLocationSelected: (LocationData) -> Unit){
+    onLocationSelected: (LocationData) -> Unit
+) {
 
-    val userLocation = remember{
+    val userLocation = remember {
         mutableStateOf(LatLng(location.latitude, location.longitude))
     }
 
-    var cameraPositionState = rememberCameraPositionState{
+    var cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(userLocation.value, 10f)
     }
 
 
-    Column(modifier= Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()) {
 
         GoogleMap(
-            modifier = Modifier.weight(1f).padding(top=16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(top = 16.dp),
             cameraPositionState = cameraPositionState,
             onMapClick = {
                 userLocation.value = it
             }
-        ){
+        ) {
             Marker(state = MarkerState(position = userLocation.value))
         }
 
@@ -50,7 +52,7 @@ fun LocationSelectionScreen(
         Button(onClick = {
             newLocation = LocationData(userLocation.value.latitude, userLocation.value.longitude)
             onLocationSelected(newLocation)
-        }){
+        }) {
             Text("Set Location")
         }
     }
